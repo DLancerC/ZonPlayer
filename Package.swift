67912c7dd8837bd6c4001b0e08c1e70600788740
Clark <dlancerc@gmail.com>
2023-12-17 04:16:09 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "ZonPlayer",
-    platforms: [.iOS(.v12)],
+    platforms: [.TVOS(.v12)],
     products: [
         .library(name: "ZonPlayer", targets: ["ZonPlayer"]),
     ],
